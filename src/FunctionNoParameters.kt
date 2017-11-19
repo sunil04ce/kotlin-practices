e5878e490e0c1d88c1ec40bffb2f5091a088ea72
Sunil Parmar <sunil04ce@gmail.com>
2017-11-19 17:48:00 +0530

    added new class

---
+++ src/FunctionNoParameters.kt
@@ -0,0 +1,13 @@
+
+fun main(args: Array<String>) {
+    helloThere()
+    printSimpleName("Sunil")
+}
+
+fun helloThere() {
+    println("Hello There!")
+}
+
+fun printSimpleName(name : String): Unit {
+    println("Name is $name")
+}
