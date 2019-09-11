63447b774208c50891387a0d812e652871eae0d2
Sunil Parmar <sunil04ce@gmail.com>
2019-09-11 11:43:28 +0530

    string demo

---
+++ src/udemy/StringDemo.kt
@@ -0,0 +1,19 @@
+package udemy
+
+fun main() {
+    val name = "Sunil Hello bro"
+    val surname = " Parmar"
+    val fullName = "$surname $name"
+    println(fullName[0])
+    println(fullName.toLowerCase())
+    println(fullName.toUpperCase())
+    println(fullName.trim())
+    println(fullName)
+
+    println("------ Split full name ------ ")
+    val tokens = fullName.trim().split(" ")
+    for(token in tokens) {
+        if(!token.contains("Hello") && !token.contains("bro"))
+            println("Token : $token")
+    }
+}
